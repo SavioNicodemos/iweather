089939430a69112c8f7ee08d516d2c6fbfdf1c1b
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-27 20:59:37 +0000

    Create centralized folder for mocks

---
+++ __tests__/mocks/mockCityApiResponse.ts
@@ -0,0 +1,8 @@
+import { CityAPIResponse } from '@services/getCityByNameService';
+
+export const mockCityApiResponse: CityAPIResponse = {
+  id: '1',
+  name: 'Mossoró',
+  sys: { country: 'BR' },
+  coord: { lon: -37.35, lat: 5.19 },
+};
--- src/services/getCityByNameService.test.ts
+++ src/services/getCityByNameService.test.ts
@@ -1,16 +1,10 @@
+import { mockCityApiResponse } from '@__tests__/mocks/mockCityApiResponse';
 import { api } from './api';
 import { getCityByNameService } from './getCityByNameService';
 
 describe('API: getCityByNameService', () => {
   it('should return city details', async () => {
-    const data = {
-      id: '1',
-      name: 'Mossoró',
-      sys: { country: 'BR' },
-      coord: { lon: -37.35, lat: 5.19 },
-    };
-
-    jest.spyOn(api, 'get').mockResolvedValue({ data });
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockCityApiResponse });
 
     const response = await getCityByNameService('Mossoró');
 
