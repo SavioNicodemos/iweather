dfc91d2c4d4f1e32ba6bdda4ceeba5cd2766d07b
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-27 21:54:23 +0000

    Test saveStorageCity

--- src/libs/asyncStorage/cityStorage.test.ts
+++ src/libs/asyncStorage/cityStorage.test.ts
@@ -1,9 +1,28 @@
-import { getStorageCity } from '@libs/asyncStorage/cityStorage';
+import {
+  getStorageCity,
+  saveStorageCity,
+} from '@libs/asyncStorage/cityStorage';
+import { CityProps } from '@services/getCityByNameService';
 
 describe('Storage: cityStorage', () => {
   it("should return null when don't have a city stored", async () => {
     const response = await getStorageCity();
 
     expect(response).toBeNull();
+  });
+
+  it('should return a city stored', async () => {
+    const newCity: CityProps = {
+      id: '1',
+      name: 'Mossoró',
+      latitude: -5.187,
+      longitude: -37.344,
+    };
+
+    await saveStorageCity(newCity);
+
+    const response = await getStorageCity();
+
+    expect(response).toEqual(newCity);
   });
 });
