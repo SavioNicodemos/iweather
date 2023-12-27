59f38366b7ed0e6123cc9923d87f603c1d237281
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-27 21:09:37 +0000

    Test getWeatherByCity with api mock

---
+++ src/services/getWeatherByCityService.test.ts
@@ -0,0 +1,17 @@
+import { mockWeatherAPIResponse } from '@__tests__/mocks/mockWeatherAPIResponse';
+import { api } from './api';
+import { getWeatherByCityService } from './getWeatherByCityService';
+
+describe('API: getWeatherByCityService', () => {
+  it('should return weather details', async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const response = await getWeatherByCityService({
+      latitude: 123,
+      longitude: 456,
+    });
+
+    expect(response).toHaveProperty('today');
+    expect(response.today).toHaveProperty('weather');
+  });
+});
