b1e3939a5e1388d596fc56e2e3f164987c224d57
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-30 02:15:22 +0000

    Add tests and functionality for rendering Dashboard screen when a city is selected

--- src/routes/routes.test.tsx
+++ src/routes/routes.test.tsx
@@ -1,11 +1,27 @@
+import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
+import { act, render, screen, waitFor } from "@__tests__/utils/customRender";
+import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { Routes } from "@routes/index";
-import { render, screen, waitFor } from "@testing-library/react-native";
+import { api } from "@services/api";
 
 describe("Routes", () => {
   it("should render Search screen when no city selected", async () => {
     render(<Routes />);
 
     const title = await waitFor(() => screen.findByText(/^escolha um local/i))
+
+    expect(title).toBeTruthy();
+  });
+
+  it("should render Dashboard screen when has a city selected", async () => {
+    jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
+
+    const city = { id: '1', name: "Mossoró", latitude: 123, longitude: 456 };
+
+    await saveStorageCity(city);
+    await act(() => waitFor(() => render(<Routes />)));
+
+    const title = screen.getByText(city.name);
 
     expect(title).toBeTruthy();
   });
