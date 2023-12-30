5374ea196fec1eceeeb62d41d151d219254b1d85
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-30 03:37:01 +0000

    Add test for showing weather of other selected city using multiple requests wait for component go out screen and manage actions

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -1,5 +1,6 @@
+import { mockCityApiResponse } from "@__tests__/mocks/api/mockCityApiResponse";
 import { mockWeatherAPIResponse } from "@__tests__/mocks/api/mockWeatherAPIResponse";
-import { render, screen, waitFor } from "@__tests__/utils/customRender";
+import { act, fireEvent, render, screen, waitFor, waitForElementToBeRemoved } from "@__tests__/utils/customRender";
 import { saveStorageCity } from "@libs/asyncStorage/cityStorage";
 import { Dashboard } from "@screens/Dashboard";
 import { api } from "@services/api";
@@ -18,4 +19,37 @@ describe('Screen: Dashboard', () => {
 
     expect(cityName).toBeTruthy();
   });
+
+  it("should show the weather of other selected city", async () => {
+    const city = {
+      id: '1',
+      name: 'Natal, RN',
+      latitude: 123,
+      longitude: 456
+    }
+
+    await saveStorageCity(city)
+
+    jest.spyOn(api, 'get')
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
+      .mockResolvedValueOnce({ data: mockCityApiResponse })
+      .mockResolvedValueOnce({ data: mockWeatherAPIResponse });
+
+    render(<Dashboard />);
+
+    await waitForElementToBeRemoved(() => screen.queryByTestId('loading'));
+
+    const cityName = 'Mossoró';
+
+    await waitFor(() => act(() => {
+      const search = screen.getByTestId('search-input');
+      fireEvent.changeText(search, cityName);
+    }));
+
+    await waitFor(() => act(() => {
+      fireEvent.press(screen.getByText(cityName, { exact: false }));
+    }));
+
+    expect(screen.getByText(cityName, { exact: false })).toBeTruthy();
+  })
 });
