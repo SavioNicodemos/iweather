be7d94f46f50f95b9b1dd41724ea0a8ddd03db92
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-12-30 03:41:06 +0000

    Refactor code duplication

--- src/screens/Dashboard/dashboard.test.tsx
+++ src/screens/Dashboard/dashboard.test.tsx
@@ -6,30 +6,23 @@ import { Dashboard } from "@screens/Dashboard";
 import { api } from "@services/api";
 
 describe('Screen: Dashboard', () => {
+  beforeAll(async () => {
+    const city = { id: '1', name: 'Natal, BR', latitude: 123, longitude: 453 };
+
+    await saveStorageCity(city);
+  });
+
   it("should show city weather", async () => {
     jest.spyOn(api, 'get').mockResolvedValue({ data: mockWeatherAPIResponse });
 
-    const city = { id: '1', name: 'Mossoró, RN', latitude: 123, longitude: 453 };
-
-    await saveStorageCity(city);
-
     render(<Dashboard />);
 
-    const cityName = await waitFor(() => screen.findByText(/Mossoró/i));
+    const cityName = await waitFor(() => screen.findByText(/Natal/i));
 
     expect(cityName).toBeTruthy();
   });
 
   it("should show the weather of other selected city", async () => {
-    const city = {
-      id: '1',
-      name: 'Natal, RN',
-      latitude: 123,
-      longitude: 456
-    }
-
-    await saveStorageCity(city)
-
     jest.spyOn(api, 'get')
       .mockResolvedValueOnce({ data: mockWeatherAPIResponse })
       .mockResolvedValueOnce({ data: mockCityApiResponse })
